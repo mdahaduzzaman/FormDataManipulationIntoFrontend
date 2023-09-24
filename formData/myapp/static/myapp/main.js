bb42993e9e765a7bf4d08316e8513dc6da3c208e
Md Ahaduzzaman <mdahaduzzamanhridoy@gmail.com>
2023-09-24 13:07:09 +0600

    edit-delete button working perfectly

--- formData/myapp/static/myapp/main.js
+++ formData/myapp/static/myapp/main.js
@@ -1,44 +1,122 @@
-let formE1 = document.querySelector('.form');
+let form = document.querySelector('.form');
 let tbody = document.querySelector('.tbody');
-const table = document.getElementsByTagName('table')
 
-formE1.addEventListener('submit', event => {
+const table = document.querySelector('table');
+let rowCounter = 1;
+
+// Convert formData to an object
+const data = [];
+
+
+// Add form data into table
+form.addEventListener('submit', event => {
     event.preventDefault();
 
-    const formData = new FormData(formE1);
+    const formData = new FormData(form);
     // Manually include checkbox values as 'on' or 'off' in the FormData
-    formE1.querySelectorAll('input[type="checkbox"]').forEach(checkbox => {
+    form.querySelectorAll('input[type="checkbox"]').forEach(checkbox => {
         formData.append(checkbox.name, checkbox.checked ? 'true' : 'false');
     });
 
-    // Convert formData to an object
-    const data = {};
+    const rowData = {};
     formData.forEach((value, key) => {
-        data[key] = value;
+        rowData[key] = value;
     });
 
-    // Create a new row and populate it with the form data
-    let tr = document.createElement('tr');
-    let emailTd = document.createElement('td');
-    let passwordTd = document.createElement('td');
-    let termsTd = document.createElement('td');
+    if(rowData['id']){
+        desiredValue = rowData['id'];
+        // Update the corresponding data entry
+        const editedRow = document.querySelector(`tr[data-row-id="${desiredValue}"]`);
+        const alltd = editedRow.childNodes;
+        
+        alltd[1].innerText =  rowData['name']
+        alltd[2].innerText =  rowData['email']
+        alltd[3].innerText =  rowData['password']
+        alltd[4].innerText =  rowData['terms']
+        
+        form.reset();
+    }else{
+        // Create a new row and populate it with the form data
+        const tr = document.createElement('tr');
+        const idTd = document.createElement('td');
+        const nameTd = document.createElement('td');
+        const emailTd = document.createElement('td');
+        const passwordTd = document.createElement('td');
+        const termsTd = document.createElement('td');
+        const actionTd = document.createElement('td');
 
-    emailTd.innerText = data.email || ''; // Use data.email to get the email value
-    passwordTd.innerText = data.password || ''; // Use data.password to get the password value
-    termsTd.innerText = data.terms || ''; // Use data.terms to get the terms value
+        // Assign a unique ID to the row
+        const rowIndex = rowCounter;
+        tr.setAttribute('data-row-id', rowIndex);
 
-    // Append the table data cells (td) to the table row (tr)
-    tr.appendChild(emailTd);
-    tr.appendChild(passwordTd);
-    tr.appendChild(termsTd);
+        idTd.innerText = rowIndex;
+        nameTd.innerText = rowData.name || '';
+        emailTd.innerText = rowData.email || '';
+        passwordTd.innerText = rowData.password || '';
+        termsTd.innerText = rowData.terms || '';
 
-    // Append the table row (tr) to the table body (tbody)
-    tbody.appendChild(tr);
+        actionTd.innerHTML = `<button class="btn btn-warning btn-edit" data-index="${rowIndex}">Edit</button>
+        <button class="btn btn-danger btn-delete" data-index="${rowIndex}">Delete</button>`;
 
-    formE1.reset()    
+        tr.appendChild(idTd);
+        tr.appendChild(nameTd);
+        tr.appendChild(emailTd);
+        tr.appendChild(passwordTd);
+        tr.appendChild(termsTd);
+        tr.appendChild(actionTd);
+
+        tbody.appendChild(tr);
+
+        // Push rowData to the data array
+        data.push(rowData);
+
+        form.reset();
+        rowCounter++;
+    }
 });
 
-let sendData = document.querySelector('.sendData')
+
+// Function to populate the edit form with existing values
+function populateEditForm(index) {
+    const item = data[index-1];
+    console.log(data)
+    console.log(item)
+    if (item) {
+        const editIndexField = document.getElementById("editIndex");
+        const editNameField = document.getElementById("editName");
+        const editPasswordField = document.getElementById("editPassword");
+        const editEmailField = document.getElementById("editEmail");
+        const editCheckField = document.getElementById("editCheck");
+
+        editIndexField.value = index;
+        editNameField.value = item.name || '';
+        editPasswordField.value = item.password || '';
+        editEmailField.value = item.email || '';
+        editCheckField.checked = item.terms || false;
+    }
+}
+
+// Event listener for the Edit button
+tbody.addEventListener("click", (event) => {
+    if (event.target.classList.contains("btn-edit")) {
+        const index = event.target.getAttribute("data-index");
+        populateEditForm(index);
+    }
+});
+
+
+// Event listener for the Delete button
+tbody.addEventListener("click", (event) => {
+    if (event.target.classList.contains("btn-delete")) {
+        const index = event.target.getAttribute("data-index");
+        // Remove the corresponding data entry
+        const deleteRow = document.querySelector(`tr[data-row-id="${index}"]`);
+        deleteRow.remove();
+    }
+});
+
+// This function will populate the total table data into input value
+let sendData = document.querySelector('.sendData');
 sendData.addEventListener('click', (e) => {
     e.preventDefault();
     let tRows = document.querySelectorAll('tr');
@@ -46,27 +124,21 @@ sendData.addEventListener('click', (e) => {
 
     tRows.forEach((row) => {
         let tData = row.querySelectorAll('td');
-
-        // Check if there are at least 3 td elements in the row
-        if (tData.length >= 3) {
+        if (tData.length >= 5) {
             let tempData = {};
 
-            tempData['name'] = tData[0].innerText;
-            tempData['email'] = tData[1].innerText;
-            tempData['terms'] = tData[2].innerText;
+            tempData['id'] = tData[0].innerText;
+            tempData['name'] = tData[1].innerText;
+            tempData['email'] = tData[2].innerText;
+            tempData['password'] = tData[3].innerText;
+            tempData['terms'] = tData[4].innerText;
 
             newData.push(tempData);
-        } else {
-            console.error('Incomplete or empty row found.');
         }
     });
+    // CONVERTING INTO STRING 
     let jsonValueToInsert = JSON.stringify(newData);
-
-    console.log(jsonValueToInsert);
-    console.log(newData);
-
-    var inputData = document.getElementById('inputData');
-
+    let inputData = document.getElementById('inputData');
     inputData.value = jsonValueToInsert;
     document.getElementById('myForm').submit();
 });
--- formData/myapp/static/myapp/new.js
+++ formData/myapp/static/myapp/new.js
@@ -1,1 +1,81 @@
-let form = document.querySelector('.form');
+/*
+        // Sample data for demonstration
+        const data = [
+            { id: 1, name: "John", age: 25, email: "john@example.com" },
+            { id: 2, name: "Jane", age: 30, email: "jane@example.com" },
+            { id: 3, name: "Doe", age: 35, email: "doe@example.com" },
+        ];
+
+        const tableBody = document.getElementById("table-body");
+        const editModal = new bootstrap.Modal(document.getElementById("editModal"));
+
+        // Function to populate the table with data
+        function populateTable() {
+            tableBody.innerHTML = "";
+
+            data.forEach((item, index) => {
+                const row = document.createElement("tr");
+                row.innerHTML = `
+                    <td>${item.id}</td>
+                    <td>${item.name}</td>
+                    <td>${item.age}</td>
+                    <td>${item.email}</td>
+                    <td>
+                        <button class="btn btn-warning btn-edit" data-index="${index}">Edit</button>
+                        <button class="btn btn-danger btn-delete" data-index="${index}">Delete</button>
+                    </td>
+                `;
+
+                tableBody.appendChild(row);
+            });
+        }
+
+        // Function to populate the edit form with existing values
+        function populateEditForm(index) {
+            const item = data[index];
+            const editIndexField = document.getElementById("editIndex");
+            const editNameField = document.getElementById("editName");
+            const editAgeField = document.getElementById("editAge");
+            const editEmailField = document.getElementById("editEmail");
+
+            editIndexField.value = index;
+            editNameField.value = item.name;
+            editAgeField.value = item.age;
+            editEmailField.value = item.email;
+        }
+
+        // Initial population of the table
+        populateTable();
+
+        // Event listener for the Edit button
+        tableBody.addEventListener("click", (event) => {
+            if (event.target.classList.contains("btn-edit")) {
+                const index = event.target.getAttribute("data-index");
+                populateEditForm(index);
+                editModal.show();
+            }
+        });
+
+        // Event listener for saving changes
+        document.getElementById("saveChanges").addEventListener("click", () => {
+            const editIndex = document.getElementById("editIndex").value;
+            const newName = document.getElementById("editName").value;
+            const newAge = document.getElementById("editAge").value;
+            const newEmail = document.getElementById("editEmail").value;
+
+            data[editIndex].name = newName;
+            data[editIndex].age = newAge;
+            data[editIndex].email = newEmail;
+            populateTable();
+            editModal.hide();
+        });
+
+        // Event listener for the Delete button
+        tableBody.addEventListener("click", (event) => {
+            if (event.target.classList.contains("btn-delete")) {
+                const index = event.target.getAttribute("data-index");
+                data.splice(index, 1);
+                populateTable();
+            }
+        });
+*/
